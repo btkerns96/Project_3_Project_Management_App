692f0f5542d7e7836eeb610cc69041b21be8b979
NikhilPrince <117137916+NikhilPrince@users.noreply.github.com>
2023-05-01 16:49:22 -0700

    home page updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,12 +14,12 @@ const Home = () => {
     <main>
       <div className="flex-row justify-center">
         <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          className="col-12 col-md- mb-3 p-3"
+          style={{ border: '4px solid #1a1a1a' }}
         >
           <ThoughtForm />
         </div>
-        <div className="col-12 col-md-8 mb-3">
+        <div className="col-12 col-md-9 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
