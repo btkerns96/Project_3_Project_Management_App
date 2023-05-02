328f1a71edc507ff1c6cf8fe18d3a2befe1a01fc
Kevin Savo <kevinsavo@Kevins-MacBook-Pro.local>
2023-05-01 18:07:14 -0700

    update

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Project Planner</h1>
+            <h1 className="m-0">Lost Projects</h1>
           </Link>
           <p className="m-0"></p>
         </div>
